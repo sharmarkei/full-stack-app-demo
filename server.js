8a2fed3917e1905a86c6cdbb03f5706552903138
sharmarke1 <sharmarkeibrahim@Sharmarke-Ibrahims-MacBook-Pro.local>
2017-06-22 13:46:46 -0700

    added notes

--- server.js
+++ server.js
@@ -6,22 +6,27 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const connectionString = 'postgres://localhost:5432/demo';
-const client = new pg.Client(connectionString);
+const connectionString = 'postgres://localhost:5432/demo'; // db name
+const client = new pg.Client(connectionString); // cretes new client inst and connects us to the db
+
+console.log('pg client obj:', client);
+
 client.connect();
 
+// MiddleWare- between the request and response, you manipulate asome stuff.  Application level
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
+//route you visit homepage, send back a response that send the index.html
 app.get('/', function(request, response) {
   response.sendFile('./public/index.html');
 });
 
 app.get('/db/person', function(request, response) {
-  client.query('SELECT * FROM persons;')
-  .then(function(data) {
-    response.send(data);
+  client.query('SELECT * FROM persons;') // select everyhitng from persons table
+  .then(function(data) { //only fire when the db returns the data. collects the data.
+    response.send(data); // sends back data to user
   })
   .catch(function(err) {
     console.err(err);
@@ -53,7 +58,7 @@ app.listen(PORT, () => {
   console.log(`currently listening on ${PORT}`);
 });
 
-function createTable() {
+function createTable() { //when you see a function declarations, they get housted.
   client.query(`
     CREATE TABLE IF NOT EXISTS persons(
       id SERIAL PRIMARY KEY,
